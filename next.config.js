60b43f4fb11b79e99aa2a7e12af938245d2c74ab
yuna1001 <hegapoo1001@gmail.com>
2022-09-19 23:40:49 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    }
-    return config
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
